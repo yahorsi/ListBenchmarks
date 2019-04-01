53f73b14e211419ebea749a82825e76c22467c90
Yahor Sinkevich <yahor.si@gmail.com>
2019-04-01 14:40:53 +0300

    Initial

---
+++ ListBenchmarks/ListBenchmarks.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+
+namespace ListBenchmarks
+{
+    public class ListBaseBenchmarks
+    {
+        private List<int> intList;
+
+        [Params(1, 5, 9)]
+        public int Index { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            intList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        }
+
+        [Benchmark]
+        public int AccessByIndex()
+        {
+            return intList[Index];
+        }
+    }
+}
